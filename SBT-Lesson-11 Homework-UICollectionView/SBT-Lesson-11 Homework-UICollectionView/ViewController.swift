b35dcf513a2b45d7cffaab18e71929a5e0303b87
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-25 22:02:35 +0300

    Added init of SBT-Lesson-11 UICollectionView

---
+++ SBT-Lesson-11 Homework-UICollectionView/SBT-Lesson-11 Homework-UICollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SBT-Lesson-11 Homework-UICollectionView
+//
+//  Created by Dmitry Shapkin on 25/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
