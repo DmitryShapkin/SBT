78d9cdd58dc5bebc7e373d4ea6c479a004e1ea31
Dmitry Shapkin <shapkindev@gmail.com>
2019-03-26 09:48:12 +0300

    Added SBT-Lesson-12 TableView-Animation

---
+++ SBT-Lesson-12 Homework-TableView-Animation/SBT-Lesson-12 Homework-TableView-Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SBT-Lesson-12 Homework-TableView-Animation
+//
+//  Created by Dmitry Shapkin on 26/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SBT-Lesson-12 Homework-TableView-Animation/SBT-Lesson-12 Homework-TableView-Animation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SBT-Lesson-12 Homework-TableView-Animation
+//
+//  Created by Dmitry Shapkin on 26/03/2019.
+//  Copyright © 2019 Dmitry Shapkin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
